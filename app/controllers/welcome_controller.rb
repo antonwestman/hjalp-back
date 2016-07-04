0263d0c46eccfda4f3337a9478fe3e517e6ae128
Anton Westman <anton.westman@apoex.se>
2016-07-04 02:02:07 +0200

    Add some stupid texts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    render text: "Hello. Downsload se app insteads"
+    render html: "<h4>Hello.</h4><p>Plz downsload se app insteads".html_safe
+  end
+
+  def registered
+    render html: "<h3>Welcomes to Hjälp.</h3><p> You's now bekräftad and ready to finds, and votes on, restaurants.<p> Open se app and vote away!".html_safe
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "welcome#index"
+  get '/registered' => 'welcome#registered'
 
   resources :food_places, only: [:index, :show]
 
