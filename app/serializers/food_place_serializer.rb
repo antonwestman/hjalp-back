63948259954f290d36b4cc40e25ae1289c1f1356
Anton Westman <anton.westman@apoex.se>
2016-07-08 17:58:54 +0200

    Add image_url and description to food_place serializer

--- app/serializers/food_place_serializer.rb
+++ app/serializers/food_place_serializer.rb
@@ -1,5 +1,12 @@
 class FoodPlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :longitude, :latitude, :genres, :rating
+  attributes :id,
+             :name,
+             :longitude,
+             :latitude,
+             :genres,
+             :rating,
+             :image_url,
+             :description
 
   def genres
     object.genres
